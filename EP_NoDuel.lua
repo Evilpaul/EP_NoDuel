2a6341afe0cebdadcd9eefcd8a89a9e997af15e4
Evilpaul <yj589794@googlemail.com>
2010-05-18 17:56:56 +0100

    consistant code style

--- EP_NoDuel.lua
+++ EP_NoDuel.lua
@@ -1,47 +1,48 @@
-local EPNoDuel = CreateFrame("Frame")
-EPNoDuel:RegisterEvent("DUEL_REQUESTED")
+local format = string.format
 
-local duelDeclineMessage = "This is an automated message, all duel requests will be declined"
+local EPNoDuel = CreateFrame('Frame')
+EPNoDuel:RegisterEvent('DUEL_REQUESTED')
+
+local duelDeclineMessage = 'This is an automated message, all duel requests will be declined'
 
 function EPNoDuel:MessageOutput(inputMessage)
-	ChatFrame1:AddMessage("|cffDAFF8A[No Duel]|r " .. inputMessage)
+	ChatFrame1:AddMessage(format('|cffDAFF8A[No Duel]|r %s', inputMessage))
 end;
 
 function EPNoDuel:DUEL_REQUESTED(event, originator)
-
 	-- Automatically cancel the duel
 	CancelDuel()
 
 	local popup
 	-- The popup is shown, make sure it goes away
 	for i = 1, STATICPOPUP_NUMDIALOGS do
-		popup = _G["StaticPopup" .. i]
+		popup = _G['StaticPopup' .. i]
 
 		if popup and 
 		   popup:IsVisible() and
-		   popup.which == "DUEL_REQUESTED" then
+		   popup.which == 'DUEL_REQUESTED' then
 			popup:Hide()
 		end
 	end
 
 	-- report to the user
-	self:MessageOutput(string.format("Declined duel with %s", originator))
+	self:MessageOutput(format('Declined duel with %s', originator))
 
 	-- only whisper if the same faction
-	local eMyFaction, _ = UnitFactionGroup("player")
+	local eMyFaction, _ = UnitFactionGroup('player')
 	local eTheirFaction, _ = UnitFactionGroup(originator)
 
 	if eMyFaction == eTheirFaction then
 		-- tell the idiot
-		SendChatMessage(duelDeclineMessage, "WHISPER", nil, originator)
+		SendChatMessage(duelDeclineMessage, 'WHISPER', nil, originator)
 	end
 end
 
-EPNoDuel:SetScript("OnEvent", function(self, event, ...)
+EPNoDuel:SetScript('OnEvent', function(self, event, ...)
 	self[event](self, event, ...)
 end)
 
 -- filter out our responses
-ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", function(_, _, msg, _)
+ChatFrame_AddMessageEventFilter('CHAT_MSG_WHISPER_INFORM', function(_, _, msg, _)
 	if msg == duelDeclineMessage then return true end
 end)
